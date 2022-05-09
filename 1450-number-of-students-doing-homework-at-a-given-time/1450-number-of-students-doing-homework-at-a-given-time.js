be2324d309a0fc31e005e8870d9bc20a5fcdaf0d
Damir Aushenov <yespeace@bk.ru>
2022-05-09 08:45:44 +0600

    Time: 55 ms (96.57%), Space: 42 MB (69.71%) - LeetHub

---
+++ 1450-number-of-students-doing-homework-at-a-given-time/1450-number-of-students-doing-homework-at-a-given-time.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} startTime
+ * @param {number[]} endTime
+ * @param {number} queryTime
+ * @return {number}
+ */
+var busyStudent = function(startTime, endTime, queryTime) {
+    var count = 0
+    for(var i = 0; i < startTime.length; i++) {
+        
+        if (startTime[i] < queryTime && endTime[i] > queryTime) {
+            count += 1
+        } else if (startTime[i] == queryTime || endTime[i] == queryTime) {
+            
+            count += 1
+        }
+      
+    }
+    
+    
+    return count
+};
