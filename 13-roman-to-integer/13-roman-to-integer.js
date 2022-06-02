d3754066630d8fe6ca3d7677cd9229684e5b08b6
Damir Aushenov <yespeace@bk.ru>
2022-06-02 21:44:34 +0600

    Time: 123 ms (93.14%), Space: 48.2 MB (32.59%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -0,0 +1,76 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    var number = 0
+    var object = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+    
+    
+    for(var i = 0; i < s.length; i++) {
+        
+        
+        
+        if (s[i] == "I" && s[i+1] == "V"  ) {
+            number += 4
+            i++
+            
+             
+            
+            
+        } else if (s[i] == "I" && s[i+1] == "X"  ) {
+              number += 9
+               i++
+          
+           
+            
+        } 
+        
+        else if (s[i] == "X" && s[i+1] == "L"  ) {
+            number += 40
+             i++
+        } 
+        
+        else if (s[i] == "X" && s[i+1] == "C") {
+             
+            number += 90
+          
+             i++
+        } 
+        
+        else if (s[i] == "C" && s[i+1] == "D") {
+             
+            number += 400
+            
+             i++
+        } 
+        
+        else if (s[i] == "C" && s[i+1] == "M") {
+             number += 900
+            i++
+           
+            
+            
+        } else {
+            number += object[s[i]]
+        }
+       
+         
+        
+        
+        
+        
+    }
+    
+   return number
+    
+    
+};
